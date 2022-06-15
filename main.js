9c87da15636a9628cb3125cfdf12290300c3f425
unknown <salim5tnt97@gmail.com>
2022-06-15 21:36:59 +0300

    four forces applied , rotate angle applied , update gui

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 import './style.css';
 import * as THREE from 'three';
-import vertexShader from './shaders/vertex.glsl';
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl';
-//import vShader from './shaders/vertex.glsl.js';
-//import fShader from './shaders/fragment.glsl.js'
-import fragmentShader from './shaders/fragment.glsl';
 import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Water } from 'three/examples/jsm/objects/Water.js';
 import { Sky } from 'three/examples/jsm/objects/Sky.js';
-import { BackSide } from 'three';
+
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import RocketBody from './assets/src/scripts/rocke_body';
 import { AxesHelper } from 'three';
-import {GUI}from 'dat.gui';
+import { GUI } from 'dat.gui';
+import { floorPowerOfTwo } from 'three/src/math/MathUtils';
 console.log(THREE);
 
 //console.log(vertexShader);
@@ -24,9 +21,9 @@ console.log(THREE);
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `
 
-let camera, scene, renderer;
+let camera, camera1, scene, renderer;
 let controls, water, sun;
-let earthMesh, cloudMesh, starMesh, group, seaEarthMesh;
+let earthMesh, cloudMesh, starMesh, group, seaEarthMesh, arrow;
 let mouse;
 //model
 let vectorRocket, groupRocket, vectorCOG, vectorCOP;
@@ -36,10 +33,14 @@ let fThrust, fWeight, mdot, rocketMass, fuelMass, fullMass, angleOfAttack, zero,
 //gravity 
 let gravityConst = 6.67428 * Math.pow(10, -11), r = 6278, earthMass = 5.97219 * Math.pow(10, 24);
 //center of pressure 
-let fDrag, drag, referenceArea, rho, dragCoefficient;
+let fDrag, drag, referenceArea, rho, dragCoefficient,liftCoefficient, theta, thetaRocket, lift, fLift;
 //euler
 let velocity, acceleration, dt = 0.01, rocketPosition;
 var control;
+
+//rotate 
+let temp, dir, temp1, length1, angle, centerOfEarth, rotateAngle;
+
 init();
 animate();
 // InitialPhyisics();
@@ -48,10 +49,13 @@ updatePhysics();
 function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 25000);
-  camera.position.z = 10;
-  camera.position.y = 5;
+  camera1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 25000);
+  camera.position.z = 1;
+  camera.position.y = 0.5;
 
-  scene.add(new THREE.AxesHelper(5000));
+  // scene.add(new THREE.AxesHelper(0.079));
+
+
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -59,18 +63,6 @@ function init() {
 
 
 
-  const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.ShaderMaterial({
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-
-    uniforms: {
-      globeTexture:
-      {
-        value: new THREE.TextureLoader().load('image/earth.jpeg')
-      }
-    }
-
-  }));
 
   const earthGeometry = new THREE.SphereGeometry(100, 378, 50, 50);
 
@@ -80,9 +72,9 @@ function init() {
     // metalness: 0,
     map: new THREE.TextureLoader().load('image/earthmap1k.jpg'),
     bumpMap: new THREE.TextureLoader().load('image/earthbump1k.jpg'),
-    bumpScale: 3.7,
+    // bumpScale: 3.7,
     specularMap: new THREE.TextureLoader().load('image/specularmap.jpg'),
-    //specular:new THREE.Color('red')
+    // specular:new THREE.Color('red')
   });
 
   // earth mesh
@@ -127,28 +119,9 @@ function init() {
 
 
 
-  //this is how to import from another java script file
-  var rocket = new RocketBody(/*put some positions*/);
-
-
-
-
-
-  //  const geometrybump=new THREE.SphereGeometry(0.5, 32, 32);
-  //  const materialbump=new THREE.MeshPhongMaterial();
-  //  const maptexture=new THREE.TextureLoader().load('image/earth.jpeg')
-  //   materialbump.map=maptexture
-  //   materialbump.bumpMap=new THREE.TextureLoader().load('image/bump.jpg')
-  //  materialbump.bumpScale=0.005
-  // materialbump.specularMap=new THREE.TextureLoader().load('image/water.png')
-  // materialbump.specular=new THREE.Color('red')
-  //  materialbump.transparent=false
-  //  materialbump.side=BackSide
-  //  const spherebump=new THREE.Mesh(geometrybump,materialbump)
-  //scene.add(spherebump)
 
   //create atmosphere 
-  const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(100, 50, 50), new THREE.ShaderMaterial({
+  const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(95, 50, 50), new THREE.ShaderMaterial({
     vertexShader: atmosphereVertexShader,
     fragmentShader: atmosphereFragmentShader,
     blending: THREE.AdditiveBlending,
@@ -157,18 +130,16 @@ function init() {
   atmosphere.scale.set(1.1, 1.1, 1.1);
   scene.add(atmosphere);
 
-  group = new THREE.Group();
-  //group.add(sphere)
-  //group.add(spherebump)
-  scene.add(group);
+  // mini window and the program still appears correctly
+  window.addEventListener('resize', function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
 
-
-
-  //animate
-  mouse = {
-    x: undefined,
-    y: undefined
-  };
+//control the camera movement
   controls = new OrbitControls(camera, renderer.domElement);
   controls.update();
 
@@ -178,7 +149,7 @@ function init() {
 
   // Water
 
-  const waterGeometry = new THREE.CircleGeometry(100, 1000);
+  const waterGeometry = new THREE.CircleGeometry(99, 100);
 
   water = new Water(
 
@@ -200,57 +171,16 @@ function init() {
   );
   // water.position.set(0, 6278, 0);
   water.rotation.x = - Math.PI / 2;
-
+  water.position.y = -1;
   scene.add(water);
 
-  // Skybox
-
-  // const sky = new Sky();
-  // sky.scale.setScalar(1);
-  // sky.geometry = new THREE.SphereGeometry(100);
-  // //scene.add( sky );
-
-  // const skyUniforms = sky.material.uniforms;
-
-  // // skyUniforms['turbidity'].value = 10;
-  // // skyUniforms['rayleigh'].value = 2;
-  // // skyUniforms['mieCoefficient'].value = 0.005;
-  // // skyUniforms['mieDirectionalG'].value = 0.8;
-
-  // const parameters = {
-  //   elevation: 2,
-  //   azimuth: 180
-  // };
-
-  // const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  
   const sky1 = new THREE.TextureLoader().load('image/Sky.jpeg');
   const geometryS = new THREE.SphereGeometry(100, 50, 50);
   const materialS1 = new THREE.MeshBasicMaterial({ map: sky1, transparent: false });
   var earth1 = new THREE.Mesh(geometryS, materialS1);
   earth1.material.side = THREE.BackSide;
   scene.add(earth1);
-
-  // function updateSun() {
-
-  //   const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
-  //   const theta = THREE.MathUtils.degToRad(parameters.azimuth);
-
-  //   sun.setFromSphericalCoords(1, phi, theta);
-
-  //   sky.material.uniforms['sunPosition'].value.copy(sun);
-  //   water.material.uniforms['sunDirection'].value.copy(sun).normalize();
-
-  //   scene.environment = pmremGenerator.fromScene(sky).texture;
-
-  // }
-
-  // updateSun();
-  // const waterUniforms = water.material.uniforms;
-
-
-
-
-
 
   // base 
   const gltfLoader = new GLTFLoader();
@@ -262,12 +192,14 @@ function init() {
       scene.add(baseRocket);
 
       baseRocket.position.set(0, 0, 0);
-      baseRocket.scale.set(4, 4, 4);
+      baseRocket.scale.set(0.04, 0.04, 0.04);
     }
   );
 
+  //models combined
   groupRocket = new THREE.Group();
-  vectorRocket = new THREE.Vector3(0, 0.1, 0);
+  //vector of the rocket
+  vectorRocket = new THREE.Vector3();
   //rocket
   gltfLoader.load('assets/models/rocket_model/scene.gltf',
     (gltf) => {
@@ -275,37 +207,30 @@ function init() {
       // console.log(rockets);
       // scene.add(rockets);
 
-      rockets.position.set(0, 3.9, 0);
-      rockets.scale.set(2, 2, 2);
+      rockets.position.set(0, 0.05, 0);
+      rockets.scale.set(0.02, 0.02, 0.02);
       groupRocket.add(rockets);
     }
   );
 
 
-
   //cylinder
-
-
-
   gltfLoader.load('assets/models/oxigen_cylinder/scene.gltf',
     (gltf) => {
       const oxigenCylinder = gltf.scene;
       // console.log(oxigenCylinder);
       //scene.add(oxigenCylinder);
 
-      oxigenCylinder.position.set(0.45, 1.5, 0);
-      oxigenCylinder.scale.set(0.15, 0.25, 0.15);
+      oxigenCylinder.position.set(0.005, 0.025, 0);
+      oxigenCylinder.scale.set(0.0015, 0.0025, 0.0015);
       groupRocket.add(oxigenCylinder);
     }
   );
   gltfLoader.load('assets/models/oxigen_cylinder/scene.gltf',
     (gltf) => {
       const oxigenCylinder1 = gltf.scene;
-      // console.log(oxigenCylinder1);
-      //scene.add(oxigenCylinder1);
-
-      oxigenCylinder1.position.set(-0.45, 1.5, 0);
-      oxigenCylinder1.scale.set(0.15, 0.25, 0.15);
+      oxigenCylinder1.position.set(-0.005, 0.025, 0);
+      oxigenCylinder1.scale.set(0.0015, 0.0025, 0.0015);
       groupRocket.add(oxigenCylinder1);
     }
   );
@@ -314,62 +239,49 @@ function init() {
 
   scene.add(groupRocket);
 
-  // addCylinder(8,30,0);
-  //addCylinder(-8,30,0);
-
-
 
   // initial value of phyisics
-  thrust = 3800000; //57800000;
   angleOfAttack = Math.PI / 2;
-  mdot = 3000;
+  // groupRocket.rotateZ( - angleOfAttack);
+  mdot = 1250;
   rocketMass = 33000;
-  fuelMass = 30000;
 
-
-  rho = 1.3;
   referenceArea = 0.112;
-  dragCoefficient = 0.75;
+  // dragCoefficient = 0.75;
   acceleration = new THREE.Vector3();
   velocity = new THREE.Vector3();
   rocketPosition = new THREE.Vector3();
   vectorCOG = new THREE.Vector3();
   vectorCOP = new THREE.Vector3();
+  centerOfEarth = new THREE.Vector3();
 
-
-  var aziz = new THREE.Vector3(2, 4, 6);
-  console.log(aziz, 'aziz');
-  //  aziz.multiplyScalar(2);
-  aziz = aziz.addScaledVector(aziz, 2);
-  console.log(aziz);
+  // var aziz = new THREE.Vector3(2, 4, 6);
+  // console.log(aziz, 'aziz');
+  // //  aziz.multiplyScalar(2);
+  // aziz = aziz.addScaledVector(aziz, 2);
+  // console.log(aziz);
 
 
   //euler
 
-//conrol -gui
-control=new function(){
-  this.thrust=3800000;
-  this.fuelMass=30000;
-  this.rho=1;
-}
-addControls(control);
-function addControls(controlObject){
-  var gui=new GUI();
-  gui.add(controlObject,'thrust',1000000,3800000)
-  gui.add(controlObject,'fuelMass',20000,50000)
-  gui.add(controlObject,'rho',0.5,1.5);
-  
-}
-fullMass = rocketMass + control.fuelMass;
-
-
-  // var gui=new GUI();
-  // var thrustGui=gui.addFolder('thrust');
-  // thrustGui.add(thrust,'thrust',thrust=100000,thrust=380000).listen();
-  // thrustGui.open();
-  // var fuelmassGui;
-  // var rhoGui;
-  
+  //conrol -gui
+  control = new function () {
+    this.thrust = 3800000;
+    this.fuelMass = 330000;
+    this.rho = 1.3;
+    this.liftCoefficient = 0.75;
+    this.dragCoefficient = 0.75;
+  };
+  addControls(control);
+  function addControls(controlObject) {
+    var gui = new GUI();
+    gui.add(controlObject, 'thrust', 0, 4200000);
+    gui.add(controlObject, 'fuelMass', 0, 50000);
+    gui.add(controlObject, 'rho', 0.5, 1.5);
+    gui.add(controlObject, 'liftCoefficient', 0.3, 1.6);
+    gui.add(controlObject, 'dragCoefficient', 0.4, 1.05);
+  }
+  fullMass = rocketMass + control.fuelMass;
 
 }
 
@@ -385,16 +297,10 @@ fullMass = rocketMass + control.fuelMass;
 
 function updatePhysics() {
 
-
-
   if (control.fuelMass == 0) {
     control.thrust = 0;
   }
 
-  // console.log(fWeight,fThrust);
-  //  console.log(groupRocket.position,fuelMass);
-  //console.log(control.thrust);
-  console.log(fThrust);
   fThrust = new THREE.Vector3(control.thrust * Math.cos(angleOfAttack), control.thrust * Math.sin(angleOfAttack), 0);
 
   weight = gravityConst * fullMass * earthMass / (r * r * 1000000);
@@ -402,24 +308,12 @@ function updatePhysics() {
   fWeight = new THREE.Vector3(0, -weight, 0);
 
 
-  drag = 0.5 * control.rho * dragCoefficient * referenceArea * velocity.length() * velocity.length();
+  drag = 0.5 * control.rho * control.liftCoefficient * referenceArea * velocity.length() * velocity.length();
   fDrag = new THREE.Vector3(-drag * Math.cos(angleOfAttack), -drag * Math.sin(angleOfAttack), 0);
 
+  lift = 0.5 * control.rho * control.dragCoefficient * referenceArea * velocity.length() * velocity.length();
+  fLift = new THREE.Vector3(-lift * Math.sin(angleOfAttack), -lift * Math.cos(angleOfAttack), 0);
 
-
-
-  // r += rocketPosition.y;
-
-
-  // console.log(vectorCOG);
-  // console.log();
-  // console.log(Math.pow(5,2));
-  // console.log(groupRocket.position);
-  // console.log(Math.cos(angleOfAttack));
-
-
-
-  // console.log(rocketPosition);
 
 
 
@@ -434,7 +328,18 @@ function updatePhysics() {
   } else {
     groupRocket.position.add(rocketPosition);
   }
-  //  console.log(groupRocket.position,rocketPosition);
+
+
+  camera1.position.set(groupRocket.position.x, groupRocket.position.y + 0.01, groupRocket.position.z + 0.2);
+ 
+
+
+  // centerOfEarth.copy(groupRocket.position);
+  // centerOfEarth.normalize();
+ 
+  
+
+  // rocketPosition.applyAxisAngle(groupRocket , 1.2);
 }
 
 
@@ -444,28 +349,73 @@ setInterval(function () {
 
 
 
+
+
+
   if (fullMass > rocketMass) {
     control.fuelMass -= mdot;
     fullMass = control.fuelMass + rocketMass;
   }
 
   if (fullMass <= rocketMass) {
-    fullMass= rocketMass;
+    fullMass = rocketMass;
     control.fuelMass = 0;
   }
 
   vectorCOG.addVectors(fWeight, fThrust);
-  vectorCOP.add(fDrag/*,fLift */);
+  //adding lift :-) + drag
+  vectorCOP.addVectors(fDrag, fLift);
   vectorRocket.addVectors(vectorCOG, vectorCOP);
 
-  acceleration = vectorRocket.divideScalar(fullMass);
+  acceleration = vectorRocket.divideScalar(fullMass * 1000);
   velocity = velocity.addScaledVector(acceleration, dt);
   rocketPosition = rocketPosition.addScaledVector(velocity, dt);
   r = groupRocket.position.y + 6278;
 
-  console.log(r);
-  // console.log(fuelMass, fullMass);
+  //velocity += acceleration *dt;
+  //position += velocity * dt;
+  temp1 = velocity;
+  length1 = temp1.length();
+  angle = 0.343 / length1;
+  //transform to radian
+  theta = angle * Math.PI / 180;
+  thetaRocket = Math.asin(theta);
+  if (!isNaN(thetaRocket) ) {
+    angleOfAttack = thetaRocket;
+  }
+  console.log(thetaRocket);
+  // console.log(thetaRocket, theta);
 
+  // console.log(groupRocket.position);
+  //arrow helpers
+  temp = new THREE.Vector3();
+  temp.copy(groupRocket.position);
+  dir = temp.normalize();
+  const origin = groupRocket.position;
+  const length = 0.05;
+  const hex = 0xffff00;
+  arrow = new THREE.ArrowHelper(dir, origin, length, hex);
+  scene.add(arrow);
+
+  // console.log('thrust : ', fThrust);
+  // console.log('weight : ', fWeight);
+  // console.log('drag : ', fDrag);
+  // console.log('lift : ', fLift);
+  // console.log('angleOfAttack:', angleOfAttack, 'theta', thetaRocket);
+  // console.log('acceleration: ', acceleration);
+  // console.log('velocity: ', velocity);
+  // console.log('rocket Position: ', groupRocket.position);
+  // console.log();
+
+  // rocketPosition.add(centerOfEarth);
+  // rotateAngle = centerOfEarth.angleTo(groupRocket.position);
+
+  // console.log(centerOfEarth,rocketPosition,rotateAngle);
+  // if (rotateAngle < 1.5707) {
+    // groupRocket.rotation.z =   -thetaRocket;
+    
+  // }
+  
 
 }, 1000);
 
@@ -490,8 +440,6 @@ function animate() {
   cloudMesh.rotation.y += 0.0010;
   starMesh.rotation.y += 0.0005;
 
-  group.rotation.y += 0.001;
-  group.rotation.y = mouse.x * 0.5;
 
 
 }
@@ -502,8 +450,4 @@ function render() {
   renderer.render(scene, camera);
 }
 
-addEventListener('mousemove', () => {
-  mouse.x = (event.clientX / innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / innerHeight) * 2 - 1;
-  // console.log(mouse)
-});
+
